07a0ca656dd73eea113cfb85757685baaaeefb7f
Daniel van den Bongard <daniel@vdbongard.com>
2017-06-24 01:47:38 +0200

    Added play, next step, randomize and clear buttons

--- src/CAEditor.js
+++ src/CAEditor.js
@@ -1,21 +1,25 @@
-import { Application, Graphics, Rectangle } from 'pixi.js'
+import { Application, Graphics, Rectangle, Container } from 'pixi.js'
 
 export default class CAEditor {
   constructor () {
     this.canvasSize = 400
     this.canvasBackgroundColor = 0xFFFFFF
     this.lineWidth = 2
-    this.lineColor = 0xCCCCCC
+    this.lineColor = 0xAAAAAA
     this.activeCellColor = 0xCCCCCC
     this.gridSize = this.canvasSize - this.lineWidth
     this.count = 20
     this.spacing = this.gridSize / this.count
     this.offset = (this.canvasSize - this.gridSize) / 2
-    this.cells = {
-      '0,0': 1,
-      '1,5': 1,
-      '9,8': 1
-    }
+    this.cells = {}
+    this.state = {}
+    this.cellContainer = new Container()
+    this.isPlaying = false
+    this.fps = 4
+    this.maxDelta = 60 / this.fps
+    this.delta = this.maxDelta
+
+
     this.app = new Application({
       width: this.canvasSize,
       height: this.canvasSize,
@@ -28,27 +32,75 @@ export default class CAEditor {
   _init () {
     this.app.view.style.userSelect = 'none'
     document.body.appendChild(this.app.view)
-
-    this._drawActiveCells()
-    this._drawGrid()
+    this.app.stage.addChild(this.cellContainer)
 
     this.app.stage.hitArea = new Rectangle(0, 0, this.canvasSize, this.canvasSize)
     this.app.stage.interactive = true
     this.app.stage.buttonMode = true
     this.app.stage.on('click', (evt) => this._onClick(evt))
     this.app.stage.on('tap', (evt) => this._onClick(evt))
+
+    this._drawGrid()
   }
 
   play () {
+    this.isPlaying = true
 
+    this.playFunction = (delta) => {
+      this.delta += delta
+      if (this.delta >= this.maxDelta) {
+        this.delta = 0
+        this.nextStep()
+      }
+    }
+
+    this.app.ticker.add(this.playFunction)
   }
 
   pause () {
+    this.isPlaying = false
+    this.delta = this.maxDelta
+    this.app.ticker.remove(this.playFunction)
+  }
 
+  nextStep () {
+    const newState = {}
+    for (let x = 0; x < this.count; x++)
+      for (let y = 0; y < this.count; y++) {
+        let neighborCount = 0
+
+        if (this.state[CAEditor._format(x - 1, y - 1)]) neighborCount++
+        if (this.state[CAEditor._format(x, y - 1)]) neighborCount++
+        if (this.state[CAEditor._format(x + 1, y - 1)]) neighborCount++
+        if (this.state[CAEditor._format(x - 1, y)]) neighborCount++
+        if (this.state[CAEditor._format(x + 1, y)]) neighborCount++
+        if (this.state[CAEditor._format(x - 1, y + 1)]) neighborCount++
+        if (this.state[CAEditor._format(x, y + 1)]) neighborCount++
+        if (this.state[CAEditor._format(x + 1, y + 1)]) neighborCount++
+
+        if (!this.state[CAEditor._format(x, y)] && (neighborCount === 3))
+          newState[CAEditor._format(x, y)] = 1
+        else if (this.state[CAEditor._format(x, y)] && (neighborCount < 2 || neighborCount > 3)) {
+        }
+        else if (this.state[CAEditor._format(x, y)])
+          newState[CAEditor._format(x, y)] = this.state[CAEditor._format(x, y)]
+      }
+    this.state = newState
+    this._drawCells()
   }
 
   randomizeCells () {
+    this.clear()
+    for (let x = 0; x < this.count; x++)
+      for (let y = 0; y < this.count; y++)
+        if (Math.random() > 0.5)
+          this._drawActiveCellAt(x, y)
+  }
 
+  clear () {
+    this.cellContainer.removeChildren()
+    this.cells = {}
+    this.state = {}
   }
 
   _onClick (evt) {
@@ -79,17 +131,8 @@ export default class CAEditor {
     this.app.stage.addChild(this.grid)
   }
 
-  _drawActiveCells () {
-    for (const k in this.cells)
-      if (this.cells.hasOwnProperty(k) && this.cells[k]) {
-        const coordinate = k.split(',')
-        const x = coordinate[0]
-        const y = coordinate[1]
-        this.cells[this._format(x, y)] = this._drawActiveCellAt(x, y)
-      }
-  }
-
   _drawActiveCellAt (x, y) {
+    console.log('new')
     const active = new Graphics()
     active.beginFill(this.activeCellColor)
     active.drawRect(
@@ -99,20 +142,39 @@ export default class CAEditor {
       this.spacing)
     active.endFill()
 
-    this.app.stage.addChild(active)
-    return active
+    this.cellContainer.addChild(active)
+    this.cells[CAEditor._format(x, y)] = active
+    this.state[CAEditor._format(x, y)] = 1
+  }
+
+  _removeActiveCellAt (x, y) {
+    this.cellContainer.removeChild(this.cells[CAEditor._format(x, y)])
+    delete this.cells[CAEditor._format(x, y)]
+    delete this.state[CAEditor._format(x, y)]
   }
 
   _toggleCellAt (x, y) {
-    if (this.cells[this._format(x, y)]) {
-      this.app.stage.removeChild(this.cells[this._format(x, y)])
-      delete this.cells[this._format(x, y)]
-    }
+    if (this.state[CAEditor._format(x, y)])
+      this._removeActiveCellAt(x, y)
     else
-      this.cells[this._format(x, y)] = this._drawActiveCellAt(x, y)
+      this._drawActiveCellAt(x, y)
   }
 
-  _format (x, y) {
+  _drawCells () {
+    for (const a in this.state)
+      if (this.state.hasOwnProperty(a) && this.state[a] && !this.cells[a]) {
+        const c = a.split(',')
+        this._drawActiveCellAt(c[0], c[1])
+      }
+
+    for (const b in this.cells)
+      if (this.cells.hasOwnProperty(b) && !this.state[b] && this.cells[b]) {
+        const c = b.split(',')
+        this._removeActiveCellAt(c[0], c[1])
+      }
+  }
+
+  static _format (x, y) {
     return `${x},${y}`
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 import CAEditor from './CAEditor'
 
-window.addEventListener('load', () => new CAEditor())
+const caEditor = new CAEditor()
+
+const playButton = document.getElementById('playButton')
+const nextStepButton = document.getElementById('nextStepButton')
+const randomizeButton = document.getElementById('randomizeButton')
+const clearButton = document.getElementById('clearButton')
+
+playButton.addEventListener('click', () => {
+  if (caEditor.isPlaying) {
+    playButton.firstChild.innerHTML = 'play_arrow'
+    caEditor.pause()
+  }
+  else {
+    playButton.firstChild.innerHTML = 'pause'
+    caEditor.play()
+  }
+})
+
+nextStepButton.addEventListener('click', () => {
+  caEditor.nextStep()
+})
+
+randomizeButton.addEventListener('click', () => {
+  caEditor.randomizeCells()
+})
+
+clearButton.addEventListener('click', () => {
+  caEditor.clear()
+})
