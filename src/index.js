a02ac9a38a09f5b18fc9574c4ab1dfe782f53e99
Daniel van den Bongard <daniel@vdbongard.com>
2017-06-29 12:55:28 +0200

    Refactor: event listener

--- src/index.js
+++ src/index.js
@@ -7,96 +7,109 @@ $(document).ready(() => { // eslint-disable-line no-undef
 
 const caEditor = new CAEditor()
 
-const playButton = document.getElementById('playButton')
-const nextStepButton = document.getElementById('nextStepButton')
-const randomizeButton = document.getElementById('randomizeButton')
-const clearButton = document.getElementById('clearButton')
-const overlappingEdgeSwitch = document.getElementById('overlappingEdgeSwitch')
-const cellCountInput = document.getElementById('cellCountInput')
-const lineWidthInput = document.getElementById('lineWidthInput')
-const survivesSelect = document.getElementById('survivesSelect')
-const bornSelect = document.getElementById('bornSelect')
-const randomProbabilityInput = document.getElementById('randomProbabilityInput')
-const fpsInput = document.getElementById('fpsInput')
+const buttons = [
+  {
+    id: 'playButton',
+    function: (domElement) => {
+      if (caEditor.isPlaying) {
+        domElement.firstChild.innerHTML = 'play_arrow'
+        caEditor.pause()
+      }
+      else {
+        domElement.firstChild.innerHTML = 'pause'
+        caEditor.play()
+      }
+    }
+  },
+  {
+    id: 'nextStepButton',
+    function: () => caEditor.nextStep()
+  },
+  {
+    id: 'randomizeButton',
+    function: () => caEditor.randomizeCells()
+  },
+  {
+    id: 'clearButton',
+    function: (domElement) => {
+      if (caEditor.isPlaying) {
+        domElement.parentElement.firstElementChild.firstChild.innerHTML = 'play_arrow'
+        caEditor.pause()
+      }
+      caEditor.clear()
+    }
+  }]
 
-playButton.addEventListener('click', () => {
-  if (caEditor.isPlaying) {
-    playButton.firstChild.innerHTML = 'play_arrow'
-    caEditor.pause()
+const toggles = [
+  {
+    id: 'overlappingEdgeSwitch',
+    name: 'overlappingEdge'
+  }]
+
+const numbers = [
+  {
+    id: 'cellCountInput',
+    name: 'count'
+  },
+  {
+    id: 'lineWidthInput',
+    name: 'lineWidth'
+  },
+  {
+    id: 'randomProbabilityInput',
+    name: 'randomProbability'
+  },
+  {
+    id: 'fpsInput',
+    name: 'fps'
+  }]
+
+const multiSelects = [
+  {
+    id: 'survivesSelect',
+    name: 'survives'
+  },
+  {
+    id: 'bornSelect',
+    name: 'born'
+  }]
+
+buttons.forEach((button) => {
+  const domElement = document.getElementById(button.id)
+  domElement.addEventListener('click', () => button.function(domElement))
+})
+
+toggles.forEach((toggle) => {
+  const domElement = document.getElementById(toggle.id)
+  domElement.checked = caEditor[toggle.name]
+  domElement.addEventListener('change', () => {
+    caEditor[toggle.name] = !!domElement.checked
+  })
+})
+
+numbers.forEach((number) => {
+  const domElement = document.getElementById(number.id)
+  domElement.value = caEditor[number.name]
+  domElement.addEventListener('input', () => {
+    const value = parseInt(domElement.value)
+    if (!isNaN(value))
+      caEditor[number.name] = value
+  })
+})
+
+multiSelects.forEach((multiSelect) => {
+  const domElement = document.getElementById(multiSelect.id)
+
+  caEditor[multiSelect.name].forEach((number) => {
+    domElement.options[number + 1].selected = true
+  })
+
+  domElement.onchange = () => {
+    const options = []
+
+    for (let i = 1; i < domElement.options.length; i++)
+      if (domElement.options[i].selected) options.push(parseInt(domElement.options[i].value))
+
+    caEditor[multiSelect.name] = options
   }
-  else {
-    playButton.firstChild.innerHTML = 'pause'
-    caEditor.play()
-  }
-})
-
-nextStepButton.addEventListener('click', () => {
-  caEditor.nextStep()
-})
-
-randomizeButton.addEventListener('click', () => {
-  caEditor.randomizeCells()
-})
-
-clearButton.addEventListener('click', () => {
-  if (caEditor.isPlaying) {
-    playButton.firstChild.innerHTML = 'play_arrow'
-    caEditor.pause()
-  }
-  caEditor.clear()
-})
-
-overlappingEdgeSwitch.checked = caEditor.overlappingEdge
-overlappingEdgeSwitch.addEventListener('change', () => {
-  caEditor.overlappingEdge = !!overlappingEdgeSwitch.checked
-})
-
-cellCountInput.value = caEditor.count
-cellCountInput.addEventListener('input', () => {
-  const number = parseInt(cellCountInput.value)
-  if (!isNaN(number))
-    caEditor.count = number
-})
-
-lineWidthInput.value = caEditor.lineWidth
-lineWidthInput.addEventListener('input', () => {
-  const number = parseInt(lineWidthInput.value)
-  if (!isNaN(number))
-    caEditor.lineWidth = number
-})
-
-for (let i = 0; i < caEditor.survives.length; i++)
-  survivesSelect.options[caEditor.survives[i] + 1].selected = true
-survivesSelect.onchange = () => {
-  const options = []
-
-  for (let i = 1; i < survivesSelect.options.length; i++)
-    if (survivesSelect.options[i].selected) options.push(parseInt(survivesSelect.options[i].value))
-
-  caEditor.survives = options
-}
-
-for (let i = 0; i < caEditor.born.length; i++)
-  bornSelect.options[caEditor.born[i] + 1].selected = true
-bornSelect.onchange = () => {
-  const options = []
-
-  for (let i = 1; i < bornSelect.options.length; i++)
-    if (bornSelect.options[i].selected) options.push(parseInt(bornSelect.options[i].value))
-
-  caEditor.born = options
-}
-
-randomProbabilityInput.value = caEditor.randomProbability
-randomProbabilityInput.addEventListener('input', () => {
-  const number = parseInt(randomProbabilityInput.value)
-  if (!isNaN(number))
-    caEditor.randomProbability = number
-})
-
-fpsInput.value = caEditor.fps
-fpsInput.addEventListener('input', () => {
-  const number = parseInt(fpsInput.value)
-  if (!isNaN(number))
-    caEditor.fps = number
 })
